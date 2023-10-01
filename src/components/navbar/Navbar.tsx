d3db844ba9db76a9c5a34f927bf5a2da80467e55
LHdensity <1252995152@qq.com>
2023-10-01 22:03:58 +0800

    导航栏 UI

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,32 @@
+import './navbar.scss'
+
+// 为什么 public 的文件在这里可以直接使用？
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="logo">
+                <img src="logo.svg" alt="" />
+                <span>hjAdmin</span>
+            </div>
+            <div className="icons">
+                <img src="/search.svg" alt="" className="icon" />
+                <img src="/app.svg" alt="" className="icon" />
+                <img src="/expand.svg" alt="" className="icon" />
+                <div className="notification">
+                    <img src="/notifications.svg" alt="" />
+                    <span>1</span>
+                </div>
+                <div className="user">
+                    <img
+                        src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+                        alt=""
+                    />
+                    <span>julien</span>
+                </div>
+                <img src="/settings.svg" alt="" className="icon" />
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
