d060ea741891b72c6611769e75ffb4e536c1312d
LHdensity <1252995152@qq.com>
2023-10-01 22:30:18 +0800

    footer UI 与 全局样式修改

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,10 @@ import './footer.scss'
 
 const Footer = () => {
     return (
-        <div className='footer'>footer</div>
+        <div className='footer'>
+            <span>hjAdmin</span>
+            <span>@Julien Dev Admin Dashboard</span>
+        </div>
     )
 }
 
