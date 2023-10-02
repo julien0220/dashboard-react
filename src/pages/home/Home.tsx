6a843c7deb3ae3a33086473a65411d1173c73b28
LHdensity <1252995152@qq.com>
2023-10-02 11:46:02 +0800

    添加barChartBox

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import './home.scss'
 import TopBox from "../../components/topBox/TopBox";
 import ChartBox from "../../components/chartBox/chartBox";
-import { chartBoxUser } from '../../../data.ts'
-import { chartBoxRevenue } from '../../../data.ts'
-import { chartBoxConversion } from '../../../data.ts'
-import { chartBoxProduct } from '../../../data.ts'
+import { chartBoxUser } from '../../../data.ts';
+import { chartBoxRevenue } from '../../../data.ts';
+import { chartBoxConversion } from '../../../data.ts';
+import { chartBoxProduct } from '../../../data.ts';
+import { barChartBoxVisit, barChartBoxRevenue } from '../../../data.ts';
+import BarChartBox from "../../components/barChartBox/BarChartBox.tsx";
 
 
 // 直接导出了一个函数，而不是一个组件？这里为什么可以这么用
 const Home = () => {
     return (
-        // <div className="home">Home</div>
         <div className="home">
             <div className="box box1">
                 <TopBox />
@@ -20,25 +21,25 @@ const Home = () => {
                 <ChartBox {...chartBoxUser} />
             </div>
             <div className="box box3">
-                <ChartBox {...chartBoxRevenue} />
-            </div>
-            <div className="box box4">
-                <ChartBox {...chartBoxConversion} />
-            </div>
-            <div className="box box5">
                 <ChartBox {...chartBoxProduct} />
             </div>
+            <div className="box box4">
+                <div />
+            </div>
+            <div className="box box5">
+                <ChartBox {...chartBoxConversion} />
+            </div>
             <div className="box box6">
-                {/* <ChartBox {...chartBoxProduct} /> */}
+                <ChartBox {...chartBoxRevenue} />
             </div>
             <div className="box box7">
-                {/* <ChartBox {...chartBoxProduct} /> */}
+                <div />
             </div>
             <div className="box box8">
-                {/* <ChartBox {...chartBoxProduct} /> */}
+                <BarChartBox {...barChartBoxRevenue} />
             </div>
             <div className="box box9">
-                {/* <ChartBox {...chartBoxProduct} /> */}
+                <BarChartBox {...barChartBoxVisit} />
             </div>
         </div>
     )
