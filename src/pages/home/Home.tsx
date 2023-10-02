1eba5cf2908201bd0f38a816767cd08082284097
LHdensity <1252995152@qq.com>
2023-10-02 11:24:00 +0800

    chartBox 添加

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,46 @@
 import React from "react";
 import './home.scss'
+import TopBox from "../../components/topBox/TopBox";
+import ChartBox from "../../components/chartBox/chartBox";
+import { chartBoxUser } from '../../../data.ts'
+import { chartBoxRevenue } from '../../../data.ts'
+import { chartBoxConversion } from '../../../data.ts'
+import { chartBoxProduct } from '../../../data.ts'
+
 
 // 直接导出了一个函数，而不是一个组件？这里为什么可以这么用
 const Home = () => {
     return (
-        <div className="home">Home</div>
+        // <div className="home">Home</div>
+        <div className="home">
+            <div className="box box1">
+                <TopBox />
+            </div>
+            <div className="box box2">
+                <ChartBox {...chartBoxUser} />
+            </div>
+            <div className="box box3">
+                <ChartBox {...chartBoxRevenue} />
+            </div>
+            <div className="box box4">
+                <ChartBox {...chartBoxConversion} />
+            </div>
+            <div className="box box5">
+                <ChartBox {...chartBoxProduct} />
+            </div>
+            <div className="box box6">
+                {/* <ChartBox {...chartBoxProduct} /> */}
+            </div>
+            <div className="box box7">
+                {/* <ChartBox {...chartBoxProduct} /> */}
+            </div>
+            <div className="box box8">
+                {/* <ChartBox {...chartBoxProduct} /> */}
+            </div>
+            <div className="box box9">
+                {/* <ChartBox {...chartBoxProduct} /> */}
+            </div>
+        </div>
     )
 }
 
